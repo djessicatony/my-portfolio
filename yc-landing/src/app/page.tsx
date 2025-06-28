8edc1c1ad5299131ec8e3ba70021265593b65998
tony <robinsanfan@mail.ru>
2025-06-29 03:54:31 +0500

    Update page.tsx

--- yc-landing/src/app/page.tsx
+++ yc-landing/src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
               </a>
               <a href="mailto:kirill@djess.net" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-muted-foreground hover:text-primary transition-colors">
                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"><path d="M1 2C0.447715 2 0 2.44772 0 3V12C0 12.5523 0.447715 13 1 13H14C14.5523 13 15 12.5523 15 12V3C15 2.44772 14.5523 2 14 2H1ZM1 3L14 3V3.92494C13.9174 3.92486 13.8338 3.94751 13.7589 3.99505L7.5 7.96703L1.24112 3.99505C1.16621 3.94751 1.0826 3.92486 1 3.92494V3ZM1 4.90797V12H14V4.90797L7.74112 8.87995C7.59394 8.97335 7.40606 8.97335 7.25888 8.87995L1 4.90797Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                <span>kirill@djess.net</span>
+                <span>namisanfan@gmail.com</span>
               </a>
             </div>
             <div className="mt-6">
@@ -76,7 +76,7 @@ export default function Home() {
           </div>
           <div className="flex-1 flex justify-center items-start pt-4">
             <Image
-              src="/luffy.jpg"
+              src="/5404744746473223637.jpg"
               alt="Profile"
               width={400}
               height={400}
