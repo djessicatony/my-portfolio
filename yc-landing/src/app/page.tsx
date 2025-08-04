378eba4fa3f13a9b878470acedbf6ac7d0afe9a2
tony <robinsanfan@mail.ru>
2025-08-05 03:30:44 +0500

    Update page.tsx

--- yc-landing/src/app/page.tsx
+++ yc-landing/src/app/page.tsx
@@ -43,10 +43,10 @@ export default function Home() {
               <h1 className="text-5xl md:text-6xl font-extrabold font-mono leading-tight flex items-center gap-3 tracking-tight text-primary">
               </h1>
               <p className="text-xl text-foreground leading-relaxed mb-4">
-                I&apos;m a high school student (graduating this year) passionate about building cool things with code :)
+                I&apos;m a co-founder of Aerovalent, passionate about building cool things with code & Travis Scott :)
               </p>
               <p className="text-lg text-foreground mb-4">
-                Currently working on @Aerovalent — An AI assistant that turns the chaos of your emails and messages into an organized list.
+                Currently working on aerovalent — cursor for your inboxes. Pulls actual tasks from your conversations to kanban-based taskboard. No overload. No dozen tabs. Just you and your tasks.
               </p>
               <p className="text-lg text-foreground mb-4">
                 Tech I use: <span className="inline-flex items-center gap-1">🐍 Python</span>, <span className="inline-flex items-center gap-1">🚀 Next.js</span>
@@ -67,7 +67,7 @@ export default function Home() {
               </a>
               <a href="mailto:kirill@djess.net" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-muted-foreground hover:text-primary transition-colors">
                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"><path d="M1 2C0.447715 2 0 2.44772 0 3V12C0 12.5523 0.447715 13 1 13H14C14.5523 13 15 12.5523 15 12V3C15 2.44772 14.5523 2 14 2H1ZM1 3L14 3V3.92494C13.9174 3.92486 13.8338 3.94751 13.7589 3.99505L7.5 7.96703L1.24112 3.99505C1.16621 3.94751 1.0826 3.92486 1 3.92494V3ZM1 4.90797V12H14V4.90797L7.74112 8.87995C7.59394 8.97335 7.40606 8.97335 7.25888 8.87995L1 4.90797Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                <span>namisanfan@gmail.com</span>
+                <span>tony@aerovalent.com</span>
               </a>
             </div>
             <div className="mt-6">
